5fcdc9f9e9cfbb3f861d1173f743169b2f734c67
Höling <Hling@192.168.0.42>
2016-10-13 18:55:29 +0200

    initial

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
+import org.jgraph.*;
 
 public class Main extends Application {
 	@Override
